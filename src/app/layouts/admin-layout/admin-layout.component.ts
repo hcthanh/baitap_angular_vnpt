724369912b9f8ba514504220393a9f31b94a83e7
thanhhc.it2 <thanhhc.it2@vnpt.vn>
2020-09-30 17:02:30 +0700

    cmit giao dien

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -155,5 +155,5 @@ export class AdminLayoutComponent implements OnInit {
       }
       return bool;
   }
-
+   
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+//khai báo biên
+  loginForm: FormGroup;
+  submitted = false;
+  isSinging = false;
+  loginData = {
+    Username: null,
+    Password: null,
+    Source: 'Web'
+  };
   constructor() { }
 
   ngOnInit(): void {
